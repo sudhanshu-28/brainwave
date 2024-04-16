93747797ff7a9458b634950da9035fe7d4198085
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-04-16 17:41:52 +0530

    Button component created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <h1 className="text-3xl underline font-bold">Hello World!</h1>;
-}
+import React from "react";
+import Button from "./components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1 className="text-3xl underline font-bold">Hello World!</h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+
+      <ButtonGradient />
+    </React.Fragment>
+  );
+};
 
 export default App;
